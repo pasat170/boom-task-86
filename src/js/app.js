4cac48be0b8e27f2a260905b0d1842c8b9fb6ff3
Stefan Poibrenski <stefan_poibrenski@abv.bg>
2023-01-16 19:32:58 +0200

    feat: uninstalling ramda

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,16 @@
 import "../scss/app.scss";
-const R = require("ramda");
-window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
 
-  const ul = document.querySelector("ul");
-  const numbersArray = [1, 3, 4, 5, 6, 7];
-  const isEven = (n) => n % 2 === 0;
-  const filteredArray = R.filter(isEven, numbersArray);
+window.addEventListener("DOMContentLoaded", () => {
+    // This block will be executed once the page is loaded and ready
+
+    const ul = document.querySelector("ul");
+    const numbersArray = [1, 3, 4, 5, 6, 7];
+    const isEven = (n) => n % 2 === 0;
+    const filteredArray = numbersArray.filter(isEven);
+
+    for (let i = 0; i < filteredArray.length; i++) {
+        const li = document.createElement("li");
+        li.textContent = filteredArray[i];
+        ul.appendChild(li);
+    }
 });
